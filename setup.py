e700b2f6bbd4a6beed68493c61e098b33239c5e2
folkemat <96705437+folkemat@users.noreply.github.com>
2023-02-26 15:14:06 +0100

    Extend setup.py for better handling of cx_freeze

--- setup.py
+++ setup.py
@@ -1,18 +1,52 @@
 import sys
 from cx_Freeze import setup, Executable
+import uuid
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["PyQt6.QtWidgets", "PyQt6.QtCore", "yaml", "chardet"]}
+upgrade_code = '{' + str(uuid.uuid4()).upper() + '}'
 
-# GUI applications require a different base on Windows (the default is for a console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+is_windows = sys.platform.startswith('win')
+
+icon_path = 'kpf.ico'
+
+script_name = "main.py"
+
+exe_name = "KoboPatchFan.exe"
+linux_name = "KoboPatchFan"
+
+# Specify the application name and version
+APP_NAME = "KoboPatchFan"
+APP_VERSION = "1.0.0"
+
+
+build_exe_options = {
+    "packages": ["PyQt6.QtWidgets", "PyQt6.QtCore", "yaml", "chardet"],
+    'excludes': ['tkinter'],
+    "include_files": [icon_path] if is_windows else []
+    }
+    
+# base="Win32GUI" should be used only for Windows GUI app
+base = "Win32GUI" if sys.platform == "win32" else None
+
+executable = Executable(script=script_name, base=base, target_name=exe_name if is_windows else linux_name, icon=icon_path if is_windows else None)
 
 setup(
-    name="KoboPatchFan",
-    version="1.0",
-    description="My PyQt6 App",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base=base, icon="kpf.ico")],
+    name=APP_NAME,
+    version=APP_VERSION,
+    description="KoboPatchFan, GUI application for Kobopatch",
+    options={
+        "build_exe": build_exe_options,  # merge the build_exe_options dictionary here
+        "bdist_msi": {
+            "initial_target_dir": r"[ProgramFilesFolder]\KoboPatchFan",
+            "add_to_path": True,
+            "all_users": True,
+            "upgrade_code": upgrade_code,  # GUID to upgrade old installations
+            'data': {
+            'Shortcut': [
+                ('DesktopShortcut', 'DesktopFolder', APP_NAME, 'TARGETDIR', '[TARGETDIR]KoboPatchFan.exe', None, None, None, None, None, None, 'TARGETDIR'),
+                ('StartMenuShortcut', 'ProgramMenuFolder', APP_NAME, 'TARGETDIR', '[TARGETDIR]KoboPatchFan.exe', None, None, None, None, None, None, 'TARGETDIR')
+                ],
+            }
+        }
+    },
+    executables=[executable],
 )
